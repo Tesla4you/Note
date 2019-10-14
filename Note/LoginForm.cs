ba5111b2f47d39e33c11fe51baf0fc0c83faf6ea
tesla4you@yandex.ru <tesla4you@yandex.ru>
2019-10-14 14:58:02 +0300

    Re

--- Note/LoginForm.cs
+++ Note/LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,19 @@ namespace Note
 {
     public partial class LoginForm : Form
     {
+        SqlConnection sqlConnection;
+        bool login = false;
+
         public LoginForm()
         {
             InitializeComponent();
-        }
-        bool login = false;
+            string connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\Note\Note\DatabaseNote.mdf;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionstring);
+        }        
 
         private void LoginForm_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void LoginForm_FormClosed(object sender, FormClosedEventArgs e)
@@ -29,11 +34,55 @@ namespace Note
             {
             Application.Exit();
             }
+            if (sqlConnection != null && sqlConnection.State != ConnectionState.Closed)
+            {
+                sqlConnection.Close();
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e) // REGISTRATION
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox1.Text) &&
+                    !string.IsNullOrEmpty(textBox2.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                SqlCommand command1 = new SqlCommand("SELECT  [Name] FROM [User] WHERE Name LIKE @Name", sqlConnection);
+
+                command1.Parameters.AddWithValue("Name", textBox1.Text.ToLowerInvariant());
+
+                SqlDataReader sqlDataReader = command1.ExecuteReader();
+
+                if (!sqlDataReader.HasRows)
+                {
+                    SqlCommand command = new SqlCommand("INSERT INTO [User] (Name, Password) VALUES(@Name, @Password)", sqlConnection);
+
+                    command.Parameters.AddWithValue("Name", textBox1.Text.ToLowerInvariant());
+
+                    command.Parameters.AddWithValue("Password", textBox2.Text);
+
+                    sqlDataReader.Close();
+
+                    await command.ExecuteNonQueryAsync();
+
+                    MessageBox.Show("Успешная Регистрация!", "Регистрация", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    MessageBox.Show("Такой пользователь уже есть!", "Регистрация", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    sqlDataReader.Close();
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Неправильно заполнены поля", "Регистрация", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e) // LOGON
         {
             
         }
+
     }
 }
